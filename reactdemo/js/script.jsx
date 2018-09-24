90feab6df60e846090b9ee696bd1f34a6a1a5e94
lflannery <31360232+lflannery@users.noreply.github.com>
2018-09-24 15:27:01 -0400

    remove jsx file

--- reactdemo/js/script.jsx
+++
@@ -1,9 +0,0 @@
-class Greeting extends React.Component {
-      render() {
-          return (<p>Hello Jessica!</p>);
-      }
-  }
-  ReactDOM.render(
-      <Greeting />,
-      document.getElementById('root')
-  );
