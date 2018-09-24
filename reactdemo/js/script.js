8c106c3431b02de8390d1501f3b4d3794627b93e
lflannery <31360232+lflannery@users.noreply.github.com>
2018-09-24 15:36:04 -0400

    Main Nav

--- reactdemo/js/script.js
+++ reactdemo/js/script.js
@@ -7,3 +7,24 @@ class Greeting extends React.Component {
       <Greeting />,
       document.getElementById('root')
   );
+
+
+class MainNav extends React.Component {
+      render() {
+          return (
+
+          <nav class="Main-Nav">
+		    <ul>
+		      <li><a href="index.html" class="Active">Home</a></li>
+		      <li><a href="blog.html">Blog</a></li>
+		      <li><a href="news.html">News</a></li>
+		    </ul>
+  		</nav>
+
+        );
+      }
+  }
+  ReactDOM.render(
+      <MainNav />,
+      document.getElementById('main-nav')
+  );
