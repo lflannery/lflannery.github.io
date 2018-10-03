53a6d9666c74542483a8b44b45a6cb884d1b8eaa
lflannery <31360232+lflannery@users.noreply.github.com>
2018-10-03 11:40:19 -0400

    Styles and header content

--- zerkcms/js/templates.js
+++ zerkcms/js/templates.js
@@ -3,37 +3,45 @@
 templates['homepage'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression;
 
-  return "<div id=\"mainHome\">\r\n	<span id=\"features\"></span>\r\n	<div class=\"Panel One\">\r\n		<div class=\"Viewport\">\r\n			<div class=\"ThreeColumn\">\r\n				<div class=\"Column One\">\r\n					"
+  return "<div class=\"Content-Header\">\r\n	<div class=\"Hero-Content\">\r\n		"
+    + ((stack1 = alias1((depth0 != null ? depth0.heroCaption : depth0), depth0)) != null ? stack1 : "")
+    + "\r\n		<img class=\"Hero-Image\" src=\""
+    + alias2(alias1((depth0 != null ? depth0.heroImage : depth0), depth0))
+    + "\" height=\"1000\" width=\"1000\" alt=\"\">\r\n		<a class=\"Button\" href=\""
+    + alias2(alias1((depth0 != null ? depth0.heroCTALink : depth0), depth0))
+    + "\">"
+    + alias2(alias1((depth0 != null ? depth0.heroCTALabel : depth0), depth0))
+    + "</a>\r\n	</div>\r\n</div>\r\n<div class=\"Content Layout-body\">\r\n	<div class=\"Viewport Main\">\r\n		<div class=\"Content-Body\">\r\n			<span id=\"features\"></span>\r\n			<div class=\"Panel One\">\r\n				<div class=\"Viewport Features\">\r\n					<div class=\"ThreeColumn\">\r\n						<div class=\"Column One\">\r\n							<h3>"
     + alias2(alias1((depth0 != null ? depth0.feature1Heading : depth0), depth0))
-    + "\r\n					"
+    + "</h3>\r\n							"
     + ((stack1 = alias1((depth0 != null ? depth0.feature1Content : depth0), depth0)) != null ? stack1 : "")
-    + "\r\n				</div>\r\n				<div class=\"Column Two\">\r\n					"
+    + "\r\n						</div>\r\n						<div class=\"Column Two\">\r\n							<h3>"
     + alias2(alias1((depth0 != null ? depth0.feature2Heading : depth0), depth0))
-    + "\r\n					"
+    + "</h3>\r\n							"
     + ((stack1 = alias1((depth0 != null ? depth0.feature2Content : depth0), depth0)) != null ? stack1 : "")
-    + "\r\n				</div>\r\n				<div class=\"Column Three\">\r\n					"
+    + "\r\n						</div>\r\n						<div class=\"Column Three\">\r\n							<h3>"
     + alias2(alias1((depth0 != null ? depth0.feature3Heading : depth0), depth0))
-    + "\r\n					"
+    + "</h3>\r\n							"
     + ((stack1 = alias1((depth0 != null ? depth0.feature3Content : depth0), depth0)) != null ? stack1 : "")
-    + "\r\n				</div>\r\n			</div>\r\n		</div>\r\n	</div>\r\n	<span id=\"clients\"></span>\r\n	<div class=\"Panel Two\">\r\n		<div class=\"Viewport\">\r\n			"
+    + "\r\n						</div>\r\n					</div>\r\n				</div>\r\n			</div>\r\n			<span id=\"clients\"></span>\r\n			<div class=\"Panel Two\">\r\n				<div class=\"Viewport\">\r\n					<h2 class=\"Page-Subtitle\">"
     + alias2(alias1((depth0 != null ? depth0.clientsPanelHeading : depth0), depth0))
-    + "\r\n			"
+    + "</h2>\r\n					"
     + ((stack1 = alias1((depth0 != null ? depth0.clientsPanelContent : depth0), depth0)) != null ? stack1 : "")
-    + "\r\n		</div>\r\n	</div>\r\n	<span id=\"salesPitch\"></span>\r\n	<div class=\"SalesPitch\">\r\n		<div class=\"Panel Three\">\r\n			<div class=\"Viewport\">\r\n				<div class=\"TwoColumn\">\r\n					<div class=\"Column One\">\r\n						"
+    + "\r\n				</div>\r\n			</div>\r\n			<span id=\"salesPitch\"></span>\r\n			<div class=\"SalesPitch\">\r\n				<div class=\"Panel Three\">\r\n					<div class=\"Viewport\">\r\n						<div class=\"TwoColumn\">\r\n							<div class=\"Column One\">\r\n								"
     + ((stack1 = alias1((depth0 != null ? depth0.salesPitch1Content : depth0), depth0)) != null ? stack1 : "")
-    + "\r\n					</div>\r\n					<div class=\"Column Two Image\">\r\n						<img src=\""
+    + "\r\n							</div>\r\n							<div class=\"Column Two Image\">\r\n								<img src=\""
     + alias2(alias1((depth0 != null ? depth0.salesPitch1Image : depth0), depth0))
-    + "\" height=\"600\" width=\"600\" alt=\"\">\r\n					</div>\r\n				</div>\r\n			</div>\r\n		</div>\r\n		<div class=\"Panel Four\">\r\n			<div class=\"Viewport\">\r\n				<div class=\"TwoColumn\">\r\n					<div class=\"Column One Image\">\r\n						<img src=\""
+    + "\" height=\"600\" width=\"600\" alt=\"\">\r\n							</div>\r\n						</div>\r\n					</div>\r\n				</div>\r\n				<div class=\"Panel Four\">\r\n					<div class=\"Viewport\">\r\n						<div class=\"TwoColumn\">\r\n							<div class=\"Column One Image\">\r\n								<img src=\""
     + alias2(alias1((depth0 != null ? depth0.salesPitch2Image : depth0), depth0))
-    + "\" height=\"600\" width=\"600\" alt=\"\">\r\n					</div>\r\n					<div class=\"Column Two\">\r\n						"
+    + "\" height=\"600\" width=\"600\" alt=\"\">\r\n							</div>\r\n							<div class=\"Column Two\">\r\n								"
     + ((stack1 = alias1((depth0 != null ? depth0.salesPitch2Content : depth0), depth0)) != null ? stack1 : "")
-    + "\r\n					</div>\r\n				</div>\r\n			</div>\r\n		</div>\r\n		<div class=\"Panel Five\">\r\n			<div class=\"Viewport\">\r\n				<div class=\"TwoColumn\">\r\n					<div class=\"Column One\">\r\n						"
+    + "\r\n							</div>\r\n						</div>\r\n					</div>\r\n				</div>\r\n				<div class=\"Panel Five\">\r\n					<div class=\"Viewport\">\r\n						<div class=\"TwoColumn\">\r\n							<div class=\"Column One\">\r\n								"
     + ((stack1 = alias1((depth0 != null ? depth0.salesPitch3Content : depth0), depth0)) != null ? stack1 : "")
-    + "\r\n					</div>\r\n					<div class=\"Column Two Image\">\r\n						<img src=\""
+    + "\r\n							</div>\r\n							<div class=\"Column Two Image\">\r\n								<img src=\""
     + alias2(alias1((depth0 != null ? depth0.salesPitch3Image : depth0), depth0))
-    + "\" height=\"600\" width=\"600\" alt=\"\">\r\n					</div>\r\n				</div>\r\n			</div>\r\n		</div>\r\n	</div>\r\n	<span id=\"contact\"></span>\r\n	<div class=\"Panel Six\">\r\n		<div class=\"Viewport\">\r\n			"
+    + "\" height=\"600\" width=\"600\" alt=\"\">\r\n							</div>\r\n						</div>\r\n					</div>\r\n				</div>\r\n			</div>\r\n			<span id=\"contact\"></span>\r\n			<div class=\"Panel Six\">\r\n				<div class=\"Viewport\">\r\n					"
     + ((stack1 = alias1((depth0 != null ? depth0.contactPanelContent : depth0), depth0)) != null ? stack1 : "")
-    + "\r\n		</div>\r\n	<div>		\r\n</div>\r\n";
+    + "\r\n				</div>\r\n			</div>\r\n		</div>\r\n	</div>		\r\n</div>\r\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
