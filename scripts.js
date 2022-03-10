12b264f413a02199371e29347fc3b50080c8f224
lflannery <31360232+lflannery@users.noreply.github.com>
2022-03-10 16:25:48 -0500

    Media queries; Mobile Nav

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-document.getElementById("year").innerHTML = new Date().getFullYear();
+$(document).ready(function() {  
+   
+    //Get current year
+    $('#year').html(new Date().getFullYear());
+
+    //Open Mobile Menu
+    $('.mobile-menu.trigger').click(function () {
+        $('.mobile-menu.nav, .mobile-menu.close').addClass('expanded');
+        $(this).addClass('hide');
+    });
+    
+    //Close Mobile Menu
+    $('.mobile-menu.close').click(function () {
+        $('.mobile-menu.nav, .mobile-menu.close').removeClass('expanded');
+        $('.mobile-menu.trigger').removeClass('hide');
+    });
+    
+
+});  
