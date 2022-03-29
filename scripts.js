8f1dcca9a839262641d574313394dc7aeffd730b
lflannery <31360232+lflannery@users.noreply.github.com>
2022-03-29 16:00:33 -0400

    Sticky Header

--- scripts.js
+++ scripts.js
@@ -15,5 +15,13 @@ $(document).ready(function() {
         $('.mobile-menu.trigger').removeClass('hide');
     });
     
+    //Sticky Header
+    $(window).scroll(function(){
+        var sticky = $('.sticky'),
+            scroll = $(window).scrollTop();
+      
+        if (scroll >= 100) sticky.addClass('fixed');
+        else sticky.removeClass('fixed');
+      });
 
 });  
